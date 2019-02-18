f65f86e0aa4a8976a0a9aa900c8f99d4d43b29f4
Tomek Stacharski <twtstacharski@gmail.com>
2019-02-18 15:56:53 +0800

    added model and controller

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,7 @@ class Listing < ApplicationRecord
   has_many :bookings
   belongs_to :user
   has_many :reviews, through: :bookings
+  has_many :wishlist_items
   mount_uploader :photo, PhotoUploader
 end
 
---
+++ test/controllers/wishlist_items_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class WishlistItemsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get wishlist_items_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get wishlist_items_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get wishlist_items_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get wishlist_items_destroy_url
+    assert_response :success
+  end
+
+end

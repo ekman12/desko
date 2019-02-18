b556f54a29ebf656b8d66fba65da21bec65162c8
vilson junior <vilsonrjunior@gmail.com>
2019-02-18 20:20:33 +0800

    updates on fb login - unfinished

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
 
  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
+#  devise_scope :user do
+#   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+# end
+
 # devise_scope :user do
 #   get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
 #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
@@ -32,27 +36,4 @@ Rails.application.routes.draw do
       resources :reviews, except: [ :edit, :update, :destroy ]
     end
   end
-
-
-
-
-
-  #       resources :reviews, only: [ :show, :index, :new, :create ]
-  #     end
-  #   end
-  # end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
-
-# root 'cocktails#index'
-#   resources :doses, only: [:destroy]
-#   resources :cocktails, except: [:edit, :update ] do
-#     resources :doses, only: [:new, :create, :destroy]
-#   end
-
-
-# IMPORTANT DEVISE MODEL
-
-# POST   /listings/:listing_id/bookings(.:format)                                                 bookings#create
-
